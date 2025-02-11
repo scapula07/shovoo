1f24a35d04b5488a02c703fbcfb39eb934fdc522
scapula07 <bartholomewonogwu@yahoo.com>
2025-02-11 15:36:14 +0100

    boiler template

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,26 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import ReactFlow, { ReactFlowProvider } from "reactflow";
+import ReactFlow, { ReactFlowProvider,Background,Controls } from "reactflow";
+// import '@xyflow/react/dist/style.css';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <div>
-       hello world
+    <div className="w-screen h-screen">
+      <ReactFlowProvider>
+        <ReactFlow 
+          fitView
+          style={{ backgroundColor: "#F7F9FB" }}
+        >
+            <Background />
+            <Controls />
+       </ReactFlow>
+
+
+              
+          </ReactFlowProvider>
     </div>
   );
 }
